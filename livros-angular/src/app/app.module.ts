17b9fac23fd8d19f77416b58f2c06d5edd464ea8
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2023-05-29 20:55:04 -0300

    Quaaaaaaaaase pronto

--- livros-angular/src/app/app.module.ts
+++ livros-angular/src/app/app.module.ts
@@ -4,14 +4,25 @@ import { LivroListaComponent } from './livro-lista/livro-lista.component';
 import { AppComponent } from './app.component';
 import { ControleEditoraService } from './controle-editora.service';
 import { ControleLivrosService } from './controle-livros.service';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { LivroDadosComponent } from './livro-dados/livro-dados.component';
+
+
 
 @NgModule({
   declarations: [
+    LivroDadosComponent,
     LivroListaComponent,
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule
+    
   ],
   providers: [ControleEditoraService, ControleLivrosService],
   bootstrap: [AppComponent]
--- livros-angular/src/app/livro-dados/livro-dados.component.ts
+++ livros-angular/src/app/livro-dados/livro-dados.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
+import { Editora } from '../editora';
+import { Livro } from '../livro';
 
 @Component({
   selector: 'app-livro-dados',
   templateUrl: './livro-dados.component.html',
   styleUrls: ['./livro-dados.component.css']
 })
-export class LivroDadosComponent {
+export class LivroDadosComponent implements OnInit {
+  public livro: Livro = new Livro(0,0,'','',[]);
+  public autoresForm: string = '';
+  public editoras: Array<Editora> = [];
 
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+  }
+
+  incluir(event: Event): void {
+    event.preventDefault();
+    this.livro.autores = this.autoresForm.split('\n');
+    this.servLivros.incluir(this.livro);
+    this.router.navigateByUrl('/lista');
+  }
 }
